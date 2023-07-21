06e43a15cd738e69755b15b4ca959044e7b74d62
benjackson3811 <benjackson3811@googlemail.com>
2023-07-21 11:52:16 +0000

    Linking index.html and base.html templates to admin

--- appointments/views.py
+++ appointments/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-import django.views import generic
-.models import Appointment
+from django.views import generic
+from .models import Appointment
+
 
 class AppointmentList(generic.ListView):
     model = Appointment
     queryset = Appointment.objects.filter(status=1).order_by('-created_on')
-    template_name ='index.html'
-    paginate_by = 10
+    template_name = 'index.html'
+    paginate_by = 12
